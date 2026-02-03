6fd080f94b1162761a97333c60bd4bd50e4ade34
satyaM singh <satyamsingh92637@gmail.com>
2026-02-03 19:48:32 +0530

    so here - i have fixed the passing of project name to every screen. 2. also make the working the recent projects section.

--- app/src/main/java/com/example/omega_v1_0/ui/screens/ProjectDashboardScreen.kt
+++ app/src/main/java/com/example/omega_v1_0/ui/screens/ProjectDashboardScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
